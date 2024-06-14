8f939b794477b61db4574c458756a2e63657f0af
johnny123214133 <zach.j.richar@gmail.com>
2024-06-13 19:23:35 -0600

    added sql commands to create and delete required tables

---
+++ mysqlSetup/create_tables/createLocation.sql
@@ -0,0 +1,8 @@
+create table if not exists location(
+	id int not null auto_increment primary key,
+	address varchar(255) not null,
+	lat decimal(10,8) not null,
+	lng decimal(11,8) not null,
+	unique(address),
+	constraint unique_coords unique(lat, lng)
+);
